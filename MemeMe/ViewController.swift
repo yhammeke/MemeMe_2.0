d2325f797b1f351352714a513b0353ff00383ec3
Yuriy Hammeke <yuriy>
2021-03-23 13:04:52 +0100

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Yuriy Hammeke on 23.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
